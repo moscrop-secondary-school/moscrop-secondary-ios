82f5cd1e07b070df4d031b991abc9d7f2c64d98f
Ivon Liu <ivon.liu2011@gmail.com>
2015-08-23 01:28:29 -0700

    Initial Commit

---
+++ MoscropSecondary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoscropSecondary
+//
+//  Created by Ivon Liu on 8/22/15.
+//  Copyright © 2015 Ivon Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+use_frameworks!
+
+target 'MoscropSecondary' do
+	pod 'Parse', '~> 1.8'
+end
+
+target 'MoscropSecondaryTests' do
+
+end
+
