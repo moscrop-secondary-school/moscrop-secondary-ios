136f54bf4b04f678e26228f7fb112ae618831cf1
Ivon Liu <ivon.liu2011@gmail.com>
2015-10-11 22:14:07 -0700

    Category seletor + subscription editor working

--- MoscropSecondary/NewsCategorySelectorTableViewController.swift
+++ MoscropSecondary/NewsCategorySelectorTableViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class NewsCategorySelectorTableViewController: UITableViewController {
 
-    var categories = [String]()
+    var subscribedCategories = [String]()
+    var allCategories = [String]()
     var selectedCategory: String?
+    
+    var editMode: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +25,17 @@ class NewsCategorySelectorTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
-        loadSubscribedCategories()
+        loadCategories()
     }
     
-    func loadSubscribedCategories() {
-        categories.removeAll(keepCapacity: false)
-        categories = ["Subscribed", "All"]
-        categories += ParseCategoryHelper.getSubscribedTagNames()
+    func loadCategories() {
+        subscribedCategories.removeAll(keepCapacity: false)
+        subscribedCategories = ["Subscribed", "All"]
+        subscribedCategories += ParseCategoryHelper.getSubscribedTagNames()
+        
+        allCategories.removeAll(keepCapacity: false)
+        allCategories += ParseCategoryHelper.getAllTagNames()
+        allCategories = allCategories.filter() { $0 != "Official" }
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,16 +54,48 @@ class NewsCategorySelectorTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return categories.count
+        if editMode {
+            return allCategories.count
+        } else {
+            return subscribedCategories.count
+        }
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("categorySelectorCell", forIndexPath: indexPath) as! NewsCategorySelectorTableViewCell
 
         // Configure the cell...
-        cell.nameLabel.text = categories[indexPath.row]
+        if editMode {
+            cell.nameLabel.text = allCategories[indexPath.row]
+            if contains(subscribedCategories, allCategories[indexPath.row]) {
+                cell.nameLabel.textColor = UIColor.blackColor()
+            } else {
+                cell.nameLabel.textColor = UIColor.lightGrayColor()
+            }
+        } else {
+            cell.nameLabel.text = subscribedCategories[indexPath.row]
+            cell.nameLabel.textColor = UIColor.blackColor()
+        }
 
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) as? NewsCategorySelectorTableViewCell {
+            if editMode {
+                if contains(subscribedCategories, allCategories[indexPath.row]) {
+                    subscribedCategories = subscribedCategories.filter() { $0 != self.allCategories[indexPath.row] }
+                    cell.nameLabel.textColor = UIColor.lightGrayColor()
+                } else {
+                    subscribedCategories += [allCategories[indexPath.row]]
+                    cell.nameLabel.textColor = UIColor.blackColor()
+                    
+                }
+            } else {
+                performSegueWithIdentifier("unwindToPostList", sender: cell)
+            }
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 
     /*
@@ -98,14 +137,52 @@ class NewsCategorySelectorTableViewController: UITableViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
-    // MARK: - Navigation
+    @IBAction func edit(sender: UIBarButtonItem) {
+        if editMode {
+            
+            // Sort the subscribed tags alphabetically
+            subscribedCategories.sort { (s1: String, s2: String) -> Bool in
+                if s1 == "Official" {
+                    return true
+                } else if s2 == "Official" {
+                    return false
+                } else {
+                    return s1 < s2
+                }
+            }
+            
+            subscribedCategories = subscribedCategories.filter() { $0 != "Subscribed" }
+            subscribedCategories = subscribedCategories.filter() { $0 != "All" }
+            
+            // Save subscribed tags
+            let preferences = NSUserDefaults.standardUserDefaults()
+            preferences.setObject(subscribedCategories, forKey: "subscribed_tags")
+            
+            subscribedCategories = ["Subscribed", "All"] + subscribedCategories
+            
+            // Update UI
+            editMode = false
+            navigationItem.leftBarButtonItem?.enabled = true
+            sender.title = "Edit"
+            tableView.reloadData()
+            
+        } else {
+            editMode = true
+            navigationItem.leftBarButtonItem?.enabled = false
+            sender.title = "Save"
+            tableView.reloadData()
+        }
+    }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // MARK: - Navigation
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
         if let selectedCell = sender as? NewsCategorySelectorTableViewCell, selectedPath = self.tableView.indexPathForCell(selectedCell) {
-            selectedCategory = categories[selectedPath.row]
+            if !editMode {
+                selectedCategory = subscribedCategories[selectedPath.row]
+            }
         }
     }
 
